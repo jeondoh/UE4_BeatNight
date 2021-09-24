6f30b231396641d8d4abd3931609f8bb159907e1
jeondoh <41669026+jeondoh@users.noreply.github.com>
2021-09-24 16:04:04 +0900

    make spider & Rat blueprints

---
+++ Source/UE4_BeatNight/Private/Rat.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Rat.h"
+
+ARat::ARat()
+{
+}
+
+void ARat::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/UE4_BeatNight/Public/Rat.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Rat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_BEATNIGHT_API ARat : public AEnemy
+{
+	GENERATED_BODY()
+
+public:
+	ARat();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	
+	
+};
